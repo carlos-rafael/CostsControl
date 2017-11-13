6f3c5fb92c307936ac8654b9fdeb6ba00976ffd7
Carlos Antunes <carlos.rafael@gec.inatel.br>
2017-11-13 21:06:54 -0200

    Add files to github

---
+++ www/js/controllers.js
@@ -0,0 +1,97 @@
+angular.module('starter.controllers', ['sqlite'])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+  Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+.controller('ListCtrl', function($scope, custosFactory) {
+  $scope.sum = 0;
+  var vm = this;
+  vm.avengers ='';
+  vm.title = 'Avengers';
+  $scope.ob = [];
+  $scope.message = '';
+  var c = activate();
+  console.log(c);
+  $scope.showMsg = false;
+
+  function activate() {
+    return custosFactory.select().then(function(data) {
+    console.log('dat vale no momento', data);
+        if (data.rows.length===0){
+          $scope.message = "Não há gastos cadastrados.";
+          console.log('dentro da func', $scope.message);
+          $scope.showMsg=true;
+        }
+        else{
+          for(var iii = 0; iii < data.rows.length; iii++){
+            console.log(data.rows.item(iii));
+            var dataFormat = data.rows.item(iii).data.split('-').reverse().join('/');
+            data.rows.item(iii).data = dataFormat;
+            console.log(typeof data.rows.item(iii).valor);
+            console.log(typeof $scope.sum);
+            $scope.sum += Number(data.rows.item(iii).valor);
+            console.log('soma é ', $scope.sum);
+            $scope.ob.push(data.rows.item(iii));
+            $scope.showMsg=false;
+            $scope.exibe=false;
+          }
+        }
+      });
+
+  }
+  console.log('depois da func', $scope.message);
+  
+  console.log(vm.avengers);
+
+})
+
+//controller do template que adiciona um novo custo
+.controller('InputCtrl', function($scope, custosFactory){
+  //inicialmente aos campos é atribuído o valor vazio
+//$scope.clearFields();
+
+  $scope.motivoCusto='';
+  $scope.valor='';
+  $scope.data='';
+  $scope.descricao='';
+  $scope.type = 'text';
+  $scope.placeholder = 'Data';
+
+  //função que limpa os campos do form
+  $scope.clearFields = function(){
+
+    $scope.motivoCusto='';
+    $scope.valor='';
+    $scope.data='';   
+    $scope.type = 'text';
+    $scope.placeholder = 'Data';  
+    $scope.descricao='';
+    $scope.teste='';
+      
+  };
+
+  //função que adiciona um novo custo
+  $scope.addCost = function(){
+
+    custosFactory.insert($scope.motivoCusto, $scope.valor, $scope.data, $scope.descricao);
+    //motivo, valor, data, descricao
+    $scope.clearFields();
+  };
+ 
+});
---
+++ www/js/sqlite.js
@@ -0,0 +1,68 @@
+var db = null;
+var sqlite = angular.module('sqlite', ['ionic', 'ngCordova']);
+
+sqlite.run(function($ionicPlatform, $cordovaSQLite){
+    $ionicPlatform.ready(function(){
+        db = $cordovaSQLite.openDB({name:"banco.db",location: 'default'});
+        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS gastos (motivo text primary key, valor decimal(8, 2), data text, descricao text)");        
+    });
+});
+
+sqlite.factory('custosFactory', function($cordovaSQLite){
+    return {
+        insert: function(motivo, valor, data, descricao){
+            var query = "INSERT INTO gastos (motivo, valor, data, descricao) VALUES (?,?,?,?)";
+            var values= [motivo, valor, data, descricao];
+
+            $cordovaSQLite.execute(db,query,values).then(
+                function(res){
+                    console.log('Success', values);
+                    alert('Gasto adicionado com sucesso!');
+                },
+                function(err){
+                    alert('Gasto adicionado com sucesso!');                    
+                    console.log('erro');
+                    console.log(err);
+                    console.log(err +' erro');
+                }
+            );
+
+        },
+        select: function(){
+            var query = "SELECT * FROM gastos";
+            
+            var teste = $cordovaSQLite.execute(db, query).then(
+                function(res){
+                    if(res.rows.length>0){
+
+                        for(var iii = 0; iii < res.rows.length; iii++){
+                            console.log(res.rows.item(iii));
+                        }
+                        var response = res.rows;
+                        console.log('response', response);
+                        
+
+
+                        //var first = res.row.item(0);
+                        var first = res.rows.item(0);
+                        console.log('Sucesso');
+                        //console.log(first);
+                        console.log('uguyguyg');
+                        //return res;
+                       // console.log(res.rows.item["[[Scopes]]"]["0"].rows[1].data);
+                    }   else{
+                        console.log('error');
+                        //return 'err';
+                    }
+                    return res;
+                } 
+                
+            );
+
+            console.log('ijfowiejfwoiefjwoeifwef ', teste);
+            return teste;
+        }
+    }
+});
+//.rows.item["[[Scopes]]"]["0"].rows["0"]
+
